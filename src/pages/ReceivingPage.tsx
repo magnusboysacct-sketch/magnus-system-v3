1f7bcef236d105836d897ddbcec071939dc35fd2
magnusboysacct-sketch <magnusboysacct@gmail.com>
2026-03-11 19:08:00 -0500

    Updated ReceivingPage.tsx

--- src/pages/ReceivingPage.tsx
+++ src/pages/ReceivingPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { supabase } from "../lib/supabase";
 import { useProjectContext } from "../context/ProjectContext";
 
